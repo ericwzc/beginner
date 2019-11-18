1b3670bab733ffa0d3c18b98857ed5a6c268a02a
bx <1564291151@qq.com>
2019-11-18 19:50:55 +0800

    syy

--- sunyingyan/1118.py
+++ sunyingyan/1118.py
@@ -9,8 +9,39 @@ def bobblesort(a):
             i += 1
         j += 1
 
+def fanzhuan(a):
+    i = 0
+    j = len(a) - 1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+    print(a)
+
+def fanzhuan2(a):
+    i = 0
+    while i < len(a) - 1 - i:
+        a[i], a[len(a) - 1 - i] = a[len(a) - 1 - i], a[i]
+        i += 1
+    print(a)
+
+def jiecheng(n):
+    if n == 1:
+        return 1
+    return n*jiecheng(n - 1)
+
+def step(n):#a+2b+3b=100,求abc的解法数量
+    if n <= 3:
+        return 7
+    return step(n - 1) + step(n - 2) + step(n - 3)
+
+def hanoi(f,t,r,n):#f是from，t是target，r是relay中转，n是盘子的数量
+    if n == 1:
+        print('{}→{}'.format(f,t))
+        return 0#如果开始只剩一个最下面的，就直接放到target
+    hanoi(f,r,t,n - 1)#把开始的除了最下面的盘子，借助relay挪到target
+    print('{}→{}'.format(f,t))#把from最下面的盘子挪到target
+    hanoi(r,t,f,n-1)# 把剩下的上面的盘子（在relay上），挪到target
 
 if __name__=='__main__':
-    a=[34,2,24,7]
-    bobblesort(a)
-    print(a)
+    hanoi('起始','终点','中转',3)
